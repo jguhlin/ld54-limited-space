729e63fefd19183c69e3ab509b6d49938177b23b
Joseph Guhlin <joseph.guhlin@gmail.com>
2023-09-30 11:19:18 +1300

    Dependencies and speed ups

---
+++ src/bin/main.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+use ld54gamelib::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+
+}
